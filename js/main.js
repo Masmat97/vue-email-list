192397065907d6cb59dfaa901543038c08be5fe2
Matteo Mascio <matteo.mascio@libero.it>
2024-05-16 10:36:19 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 const { createApp } = Vue;
 
 createApp({
-data() {
-    return {
-        message: "Generare 10 email con API",
-        mail: "https://flynn.boolean.careers/exercises/api/random/mail",
-        newmail: [],
+    data() {
+        return {
+            message: "Generare 10 email con API",
+            mail: "https://flynn.boolean.careers/exercises/api/random/mail",
+            newmail: [],
+        }
+    },
+    methods: {
+        getmail() {
+            this.newmail = []
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.mail).then((result) => {
+                    console.log(result.data.response);
+                    this.newmail.push(result.data.response);
+                })
+            }
+
+
+        }
+    },
+    mounted() {
+        console.log("app caricata");
+
     }
-},
- methods: {
-    getmail() {
-        axios.get(this.mail).then((result) =>{
-            console.log(result.data.response);
-            this.newmail.push(result.data.response);
-        })
-    }
- },
- mounted() {
-    console.log("app caricata");
-    for (let i = 0; i < 10; i++){
-        this.getmail()
-    }
- }
- 
+
 
 
 }).mount("#app")
